a22aa524c5bdb000497d311540c47af425e09d95
Iftakhar Ahmed <iftakharn19@gmail.com>
2021-02-22 03:20:46 +0600

    add questions of the exercise 6

---
+++ Exercise set 6 - SQL basics 5/Exercise set 6 - SQL basics 5.sql
@@ -0,0 +1,20 @@
+-- Iftakhar Husan
+
+-------------------------------------------------------------------------
+
+-- 1 - Insert two new rows for SALARIES table:
+--      3500 euros for employee 10
+--      3300 euros for employee 20
+
+
+-- 2 - Change the salary for employee 20 so that it will be 3600 euros.
+
+
+-- 3 - Create a new table called SALARYHISTORY with similar structure as SALARY table.
+
+
+-- 4 - Copy the content of SALARY table to SALARYHISTORY with one query.
+
+
+-- 5 - Remove the salary information of employee 20 from SALARY and SALARYHISTORY tables.
+
