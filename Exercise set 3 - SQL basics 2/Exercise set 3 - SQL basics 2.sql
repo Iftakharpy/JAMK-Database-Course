7237be1372210987e54f3dadf6d976a980ee067b
Iftakhar Ahmed <iftakharn19@gmail.com>
2021-02-22 01:33:02 +0600

    final check for exercise 3

--- Exercise set 3 - SQL basics 2/Exercise set 3 - SQL basics 2.sql
+++ Exercise set 3 - SQL basics 2/Exercise set 3 - SQL basics 2.sql
@@ -13,10 +13,14 @@ SELECT ROUND(AVG(rate), 2) AS Average FROM emps WHERE dept BETWEEN 'M' AND 'P';
 
 -- 2 - Select employee count of employees having rate value between 6-8.
 --      Set result set column name to be EmpRateCount.
-SELECT COUNT(rate) AS EmpRateCount FROM emps WHERE rate BETWEEN 6 AND 8;
+SELECT COUNT(empnum) AS EmpRateCount FROM emps WHERE rate BETWEEN 6 AND 8;
+-- Employee counts for different rates
+SELECT rate, COUNT(empnum) AS EmpRateCount FROM emps WHERE rate BETWEEN 6 AND 8 GROUP BY rate ORDER BY rate;
 
 -- 3 - Count the sum for rate values of employees working in departments A-G?
 SELECT SUM(rate) AS Total_rate FROM emps WHERE dept BETWEEN 'A' AND 'G';
+-- Rate sum for individual departments
+SELECT dept, SUM(rate) AS Total_rate FROM emps WHERE dept BETWEEN 'A' AND 'G' GROUP BY dept ORDER BY dept;
 
 -- 4 - Select the maximum and minimum value of rate value from employees living in Davis.
 --       Set result set column names to be Greatest and Smallest.
@@ -42,16 +46,27 @@ SELECT dept, COUNT(empnum) AS Count
 --         - last three letters from surname and
 --         - first three numbers from phone number.
 --      Use column name Username.
-SELECT CONCAT(LEFT(gname, 2), RIGHT(surname, 3), LEFT(phone, 3)) AS Username,
+-- COALESCE() -- doc: https://docs.microsoft.com/en-us/sql/t-sql/language-elements/coalesce-transact-sql?view=sql-server-ver15
+-- Used COALESCE to prevent setting username to NULL when one column's value is NULL
+-- solve 1
+SELECT CONCAT(LEFT(gname, 2), RIGHT(surname, 3), LEFT(phone, 3)) AS Username FROM emps;
+-- solve 2
+SELECT CONCAT(COALESCE(LEFT(gname, 2), ''),
+            COALESCE(RIGHT(surname, 3), ''),
+            COALESCE(LEFT(phone, 3), '')) AS Username,
         gname AS firstname,
         surname,
         phone 
-    FROM emps; -- solve 1
-SELECT CONCAT(SUBSTRING(gname, 1, 2), SUBSTRING(surname, -3, 3), SUBSTRING(phone, 1, 3)) AS Username,
+    FROM emps;
+-- solve 3
+SELECT CONCAT(COALESCE(SUBSTRING(gname, 1, 2), ''),
+                COALESCE(SUBSTRING(surname, -3, 3), ''),
+                COALESCE(SUBSTRING(phone, 1, 3), '')) AS Username,
         gname AS firstname,
         surname,
         phone 
-    FROM emps; -- solve 2
+    FROM emps;
+
 
 -- 7 - Select different cities and show city names in lowercase letters in the result set.
 --      Use column name Locations.
@@ -61,8 +76,9 @@ SELECT DISTINCT(LOWER(city)) AS Locations FROM emps ORDER BY Locations ASC;
 -- 8 - Select employee firstname, surname and the length of surname (use column name surname_length).
 --      Include only employees whose surname length is greater than six characters.
 SELECT gname AS firstname, surname, LENGTH(surname) AS surname_length 
-	FROM emps 
-	WHERE LENGTH(surname)>6;
+    FROM emps 
+    WHERE LENGTH(surname)>6
+    ORDER BY surname_length;
 
 
 ---------------------------------------------------------------------------------------------------
@@ -108,7 +124,8 @@ FROM TargetTable
 /*  
 ------------------------- What the SELECT query returns -----------------------------
 
-Initially, I replaced "TargetTable" with "sdept" the result was 
+1. Initially, I replaced "TargetTable" with "sdept".
+    The query produced the following result: 
     `
     CURRENT_USER    |   CheckIt
     ----------------|-----------
@@ -117,16 +134,23 @@ Initially, I replaced "TargetTable" with "sdept" the result was
     root@localhost	|   Late
     `
     This is because sdept table had 3 records. 
-    As, CURRENT_DATE is '2021-02-19' which is greater than '2011-02-22' so CheckIt is Late for every column.
-    The CURRENT_USER was 'root@localhost' and that remains the same untill a query returns the results.
+    As, CURRENT_DATE is '2021-02-19' which is greater than '2011-02-22' so CheckIt is 'Late' for every column.
+    The CURRENT_USER was 'root@localhost' and that remains the same if someone doesn't change one's username.
 
-In the next turn, I created a table and named it TargetTable. Then inserted a record.
-After that I executed the query and the result was
-`
-CURRENT_USER    |   CheckIt
-----------------|-----------
-root@localhost	|   Late
-`
+2. In the next turn, I created a table and named it 'TargetTable' with 'name' attribute. 
+    Then inserted a record with value 'Iftakhar'.
+    Using the commands below:
+
+    CREATE TABLE TargetTable(name VARCHAR(100));
+    INSERT INTO TargetTable(name) VALUES('Iftakhar'); 
+
+    After that I executed the query mantioned in the task 11 as it is. 
+    It returned the following output:
+    `
+    CURRENT_USER    |   CheckIt
+    ----------------|-----------
+    root@localhost	|   Late
+    `
 
 
 ------------------------- Analysis for the Why -----------------------------
@@ -139,7 +163,8 @@ In the SELECT query CURRENT_USER and CheckIt columns are returned in the result 
     The number of rows depends on the number of rows stored in the "TargetTable".
     It will return the same number of rows stored in the "TargetTable".
         If "TargetTable" has 0 records then the query won't return any result.
-    The result will always be the same.
+    The result will always be the same. 
+    Unless, someone changes the system datetime and the logged in user.
     
     CheckIt column's values will depend on `CAST(CURRENT_DATE AS CHAR(10)) <= '2011-02-22'` comparison operation.
     If the current date is smaller than or equals to the string '2011-02-22' then the value would be 'OK'.
@@ -147,5 +172,5 @@ In the SELECT query CURRENT_USER and CheckIt columns are returned in the result 
     
     If the "TargetTable" doesn't exist in the currently selected database error will occur.
     The error message will be something like this `Error Code: 1146. Table 'exercise.TargetTable' doesn't exist`
-    All of the columns will be indentical in the result set.
+    All of the columns will be identical in the result set.
 */
